e24286466a48358128ab25656ea47b7b1e531d0f
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2020-11-15 18:35:39 -0300

    chg: add filter`s on findAll at IRoverRepository

--- src/application/Interfaces/rover-repository.interface.ts
+++ src/application/Interfaces/rover-repository.interface.ts
@@ -1,8 +1,12 @@
-import { Rover } from "src/domain";
+import { Coordinate, Rover } from "../../domain";
 
 export interface IRoverRepository {
   insert(rover: Rover): Promise<Rover>
   update(rover: Rover): Promise<Rover>
   findById(id: string): Promise<Rover>
-  findAll(): Promise<Rover[]>
+  findAll(filters?: IRoverRepositoryFilters): Promise<Rover[]>
+}
+
+export interface IRoverRepositoryFilters {
+  coordinate?: Coordinate;
 }
