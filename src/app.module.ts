83f5f19b04dcd707468099a03879cd4e60b87948
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2020-11-29 20:54:45 -0300

    new: plateau module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PlateauModule } from './plateau/plateau.module';
 import { RoverModule } from './rover/rover.module';
 
 @Module({
   imports: [
+    PlateauModule,
     RoverModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
