91ab51f7b3e3cfa5b659351921230751a7a2d0c2
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2020-11-30 00:52:04 -0300

    fix: rover repository

--- src/rover/ports/persistence/rover.repository.ts
+++ src/rover/ports/persistence/rover.repository.ts
@@ -6,19 +6,19 @@ import { Rover } from "../../domain";
 export class RoverRepository implements IRoverRepository {
   async insert(rover: Rover): Promise<Rover> {
     rovers.push(rover);
-    return this.findById(rover.getId());
+    return this.findById(rover.id);
   }
 
   async update(rover: Rover): Promise<Rover> {
     const index = rovers.findIndex(
-      r => r.getId() === rover.getId()
+      r => r.id === rover.id
     );    
     rover[index] = rover;    
-    return this.findById(rover.getId());
+    return this.findById(rover.id);
   }
 
   async findById(id: string): Promise<Rover> {
-    return rovers.find(r => r.getId() === id);
+    return rovers.find(r => r.id === id);
   }
 
   async findAll(filters?: IRoverRepositoryFilters): Promise<Rover[]> {
