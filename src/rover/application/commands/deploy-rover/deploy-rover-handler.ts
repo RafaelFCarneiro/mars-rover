52f46860cf928801ecbf53bfc005c8f2fc1bd283
rafaelfcarneiro <rafaferreiracarneiro@gmail.com>
2024-03-30 17:44:25 -0300

    refactor: indentation

--- src/rover/application/commands/deploy-rover/deploy-rover-handler.ts
+++ src/rover/application/commands/deploy-rover/deploy-rover-handler.ts
@@ -1,51 +1,52 @@
 import { CommandHandler, EventPublisher, ICommandHandler } from "@nestjs/cqrs";
 import { Inject } from "@nestjs/common";
 import { DeployRoverCommand } from "./deploy-rover-command";
-import { DIIdentifiers, IRoverRepository } from '../../Interfaces';
-import { RoverDto } from '../../dtos';
-import { 
-  Coordinate, 
-  Plateau, 
-  Rover, 
-  RoverLocation, 
-  RoverOrientation, 
-  RoverOrientationType
+import { DIIdentifiers, IRoverRepository } from "../../Interfaces";
+import { RoverDto } from "../../dtos";
+import {
+  Coordinate,
+  Plateau,
+  Rover,
+  RoverLocation,
+  RoverOrientation,
+  RoverOrientationType,
 } from "../../../domain";
 
 @CommandHandler(DeployRoverCommand)
 export class DeployRoverHandler implements ICommandHandler<DeployRoverCommand> {
   constructor(
-    @Inject(DIIdentifiers.IRoverRepository) private readonly repo: IRoverRepository,
-    private readonly publisher: EventPublisher,
+    @Inject(DIIdentifiers.IRoverRepository)
+    private readonly repo: IRoverRepository,
+    private readonly publisher: EventPublisher
   ) {}
 
-  async execute(command: DeployRoverCommand) {    
+  async execute(command: DeployRoverCommand) {
     const { id, plateau, position } = command;
-    
-    const orientation = !!command?.orientation 
+
+    const orientation = !!command?.orientation
       ? RoverOrientationType[command?.orientation]
       : RoverOrientationType.N;
 
-    const rover = await this.repo.findById(id) ?? new Rover(id);
+    const rover = (await this.repo.findById(id)) ?? new Rover(id);
     if (rover.isDeployed()) {
       throw new Error(DeployRoverErrors.RoverAlreadyDeployed);
     }
 
     rover.deploy({
       location: new RoverLocation({
-        plateau: new Plateau(plateau), 
-        coordinate: new Coordinate(position)
+        plateau: new Plateau(plateau),
+        coordinate: new Coordinate(position),
       }),
-      orientation: new RoverOrientation({ value: orientation })
+      orientation: new RoverOrientation({ value: orientation }),
     });
-    
+
     const updatedRover = this.publisher.mergeObjectContext(
       await this.repo.update(rover)
     );
-    
+
     updatedRover.commit();
- 
-    return { 
+
+    return {
       ...updatedRover,
       location: rover.getLocation(),
       position: rover.getPosition(),
@@ -55,5 +56,5 @@ export class DeployRoverHandler implements ICommandHandler<DeployRoverCommand> {
 }
 
 export const DeployRoverErrors = {
-  RoverAlreadyDeployed: 'Rover is already deployed.',
-}
+  RoverAlreadyDeployed: "Rover is already deployed.",
+};
